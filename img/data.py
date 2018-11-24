d9bb15f67b8b286035f09680d68587c33d6a464a
PavelVo <pavel.v@tut.by>
2018-11-24 13:38:48 +0300

    add img

---
+++ img/data.py
@@ -0,0 +1,13 @@
+
+def img_p1():
+    fold = ('img/p1/fold.png', 'Fold')
+    postSB = ('img/p1/postSB.png', 'PostSB')
+    postBB = ('img/p1/postBB.png', 'PostBB')
+    call = ('img/p1/call.png', 'Call')
+    check = ('img/p1/check.png', 'Check')
+    rise = ('img/p1/raise.png', 'Raise')
+    muck = ('img/p1/muck.png', 'Muck')
+    bet = ('img/p1/bet.png', 'Bet')
+    
+    names=(fold, postSB, postBB, call, check, rise, muck, bet)
+    return names
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print('hello world')
+#%%
+from img.data import *
+from models.create import *
+
+x = img_p1()
+new_walk_model(x)
