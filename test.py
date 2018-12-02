078ae0d246a3d4f352da4bb9721553f9a7858239
PavelVo <pavel.v@tut.by>
2018-12-02 14:19:25 +0300

    tst

---
+++ test.py
@@ -0,0 +1,42 @@
+#%%
+import shutil
+from engine.processes import *
+from models.data import *
+import time
+# image = scrshot(64, 53, 140, 68) # hand
+# l = create_model(image, 'hand', 'hand')
+# image
+
+# image = scrshot(80, 157, 135, 195) # empty
+
+# # empty = lern_model('empty')
+# ret, results, neighbours, dist = check_image(image, empty)
+# image
+
+# img= scrshot(46, 157, 135, 177) #p1 walk
+# img
+# new = walk(img)
+# new
+
+# img= scrshot(350, 90, 435, 110) #p2 walk
+# # img
+# new = walk(img)
+# new
+shutil.rmtree('models/hand')
+image = scrshot(64, 53, 140, 68) # hand
+create_model(image, 'hand', 'hand')
+hand = lern_model('hand')
+ret, results, neighbours, dist = check_image(image, hand)
+for x in range(50):
+    image = scrshot(64, 53, 140, 68) # hand
+    ret, results, neighbours, dist = check_image(image, hand)
+    if dist[0][0] > 0:
+        shutil.rmtree('models/hand')
+        create_model(image, 'hand', 'hand')
+        hand = lern_model('hand')
+        print('new hand')
+    print('last hand')
+    time.sleep(3)
+
+
+
